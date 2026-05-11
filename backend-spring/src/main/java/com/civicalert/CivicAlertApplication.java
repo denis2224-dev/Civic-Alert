5f2906ced0be0450b6a387d5f4297499aff32ef0
Denis <denymoroz2006@gmail.com>
2026-05-11 18:42:58 +0300

    Configure Spring Boot with PostgreSQL

---
+++ backend-spring/src/main/java/com/civicalert/CivicAlertApplication.java
@@ -0,0 +1,13 @@
+package com.civicalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivicAlertApplication.class, args);
+    }
+}
+
